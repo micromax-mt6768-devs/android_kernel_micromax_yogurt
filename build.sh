Micromax-G85/kernel_micromax_E7746

scripts: add build script
Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

build.sh
@@ -1,0 +1,42 @@
+#!/bin/bash
+
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=ohing
+export KBUILD_BUILD_USER="sakthivel"
+git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+
+[ -d "out" ] && rm -rf out || mkdir -p out
+
+make O=out ARCH=arm64 E7746_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git -b E7746 AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+nano anykernel.sh
+zip -r9 StormBreaker-r1.0-E7746.zip *
+curl -sL https://git.io/file-transfer | sh
+./transfer wet StormBreaker-r1.0-E7746.zip
+}
+
+compile
+zupload

micromax-mt6768-devs/android_kernel_micromax_yogurt

build.sh
@@ -1,0 +1,41 @@
+#!/bin/bash
+
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=ohing
+export KBUILD_BUILD_USER="sakthivel"
+git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+
+[ -d "out" ] && rm -rf out || mkdir -p out
+
+make O=out ARCH=arm64 E7746_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/SakthivelNadar/AnyKernel3-2.git AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 StormBreaker-r1.0-yogurt.zip *
+curl -sL https://git.io/file-transfer | sh
+./transfer wet StormBreaker-r1.0-yogurt.zip
+}
+
+compile
+zupload